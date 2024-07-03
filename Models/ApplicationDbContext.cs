e96f7473ceaad6f896f27c42975a46ea24516bbd
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2024-07-03 13:46:01 +0900

    Feat: 데이터베이스 연결 및 Member 테이블 구성

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPCSpec.Models.DAO;
+
+namespace MyPCSpec.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet 정의
+        public DbSet<Member> Member { get; set; }
+    }
+}
