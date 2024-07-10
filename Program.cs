c8766c6c353b5c25b6be429fc6bba9fc81d1490e
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2024-07-10 23:58:20 +0900

    프로그램 로깅 추가

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@ using Microsoft.EntityFrameworkCore;
 using MyPCSpec.Models;
 using MyPCSpec.Services;
 using MyPCSpec.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// 로깅 설정 추가
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
@@ -20,12 +25,13 @@ builder.Services.AddDbContext<MpsContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("MPS_Database"),
     new MySqlServerVersion(new Version(8, 0, 21))));
 
-//서비스 등록
+// 서비스 등록
 builder.Services.AddScoped<IMemberService, MemberService>();
 builder.Services.AddScoped<ILoginHistoryService, LoginHistoryService>();
 
 var app = builder.Build();
 
+// 환경에 따른 설정
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -45,4 +51,13 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+try
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred while starting the application.");
+    throw;
+}
